7cebb67156df25e86e5761286d37c450dd33f1cf
caleb650 <caleb170506@gmail.com>
2025-07-31 14:08:38 +0530

    code7

---
+++ program 7.py
@@ -0,0 +1,8 @@
+print("circle program")
+print("--------------")
+A=int(input("Enter the A value"))
+print("Result")
+B=2*22/7*A
+print("perimeter of circle:",B)
+C=22/7*A*A
+print("Aera of circle:",C)
